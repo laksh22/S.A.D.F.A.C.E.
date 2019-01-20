d7c3c46b440453ac9b397c1c9fb205441231c5c2
Simeng Han <hans0035@e.ntu.edu.sg>
2019-01-20 10:19:46 +0800

    update

--- Emotion Recognition Workaround/api.js
+++ Emotion Recognition Workaround/api.js
@@ -1,15 +1,14 @@
 
 // POST to httpbin which returns the POST data as JSON
-function myFunc(){
-    const Http = new XMLHttpRequest();
+
+const Http = new XMLHttpRequest();
 let data = new FormData();
-data.append(
-    'link', tablink
-);
-const url='http://127.0.0.1:5000/';
+console.log(tablink);
+data.append('link', tablink);
+const url='http://127.0.0.1:5000/go/';
 Http.open("POST", url);
 Http.send(data);
 Http.onreadystatechange=(e)=>{
 console.log(Http.responseText)
 };
-}
+
